d0d9df82e6b76b99a2cf65ae23745e8d839f3f59
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2024-01-11 22:44:42 +0530

    Added navbar changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { useState } from "react";
 import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
 import "./assets/css/General.css";
-import CardPage from "./components/CardPage";
-import NameCard from "./components/NameCard";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -16,19 +14,24 @@ function App() {
       </div>
       <div className="content_container">
         <div className="main_container">
-            <div className="left">
-                <h1 className="heading">I turn ideas into reality</h1>
-                <h3 className="sub_heading">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta est ipsum recusandae impedit quaerat quidem suscipit debitis 
-                </h3>
-            </div>
-            <div className="right"></div>
+          <div className="left">
+            <h1 className="heading">I turn ideas into reality</h1>
+            <h3 className="sub_heading">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              est ipsum recusandae impedit quaerat quidem suscipit debitis
+            </h3>
+            <button className="custom_btn">
+              Let's Collaborate&ensp;
+              <i class="fa-solid fa-arrow-right-long"></i>
+            </button>
+          </div>
+          <div className="right"></div>
         </div>
         <div className="curve_background"></div>
       </div>
       <div className="footer_container">
         <div className="main_container">
-            <div className="social_section"></div>
+          <div className="social_section"></div>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,23 @@ export default function Navbar() {
   return (
     <>
       <div className="main_container">
-        <div className="section section1"></div>
-        <div className="section section2"></div>
-        <div className="section section3"></div>
+        <div className="section section1">
+            <img src="./images/logo/Icon 1.png" alt="" />
+        </div>
+        <div className="section section2">
+            <span>
+                <a href="">Home</a>
+            </span>
+            <span>
+                <a href="">About</a>
+            </span>
+            <span>
+                <a href="">Blogs</a>
+            </span>
+            <span>
+                <a href="">Contact Us</a>
+            </span>
+        </div>
       </div>
     </>
   );
